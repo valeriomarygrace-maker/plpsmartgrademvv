af100e1110ba2b031d320cf9e32d45e051e067b3
marygracevalerio <valerio_marygrace@plpasig.edu.ph>
2025-11-13 17:38:48 +0800

    config fix

--- logout.php
+++ logout.php
@@ -29,34 +29,3 @@ header("Location: login.php");
 exit();
 ?>
 
-<?php
-// Start session if not already started
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-// Destroy all session variables
-$_SESSION = array();
-
-// If it's desired to kill the session, also delete the session cookie
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-
-// Finally, destroy the session
-session_destroy();
-
-// Clear any cached pages
-header("Cache-Control: no-cache, no-store, must-revalidate");
-header("Pragma: no-cache");
-header("Expires: 0");
-
-// Redirect to login page
-header("Location: login.php");
-exit();
-?>
-
